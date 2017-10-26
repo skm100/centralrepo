cf729d6df47fc01c49c1f152fedaf1fcdcccd68d
skm100 <33090409+skm100@users.noreply.github.com>
2017-10-26 11:30:41 +0530

    Added one more method

--- Hello.java
+++ Hello.java
@@ -1,8 +1,11 @@
 puvlic class Hello
 {
-  public ststic void main(String args[])
+  public static void main(String args[])
   {
      System.out.println("Hello Everyone !!!");
+  }
+  public static void sayHello(){
+    System.out.println("Hello DevOps");
   }
 }
   
